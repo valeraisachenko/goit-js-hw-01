03dac56a5b212610b62e83f8ea4e3f693ea6f1ae
Valeriy <49371436+valeraisachenko@users.noreply.github.com>
2023-07-16 22:16:46 +0200

    1 task complit

---
+++ js/task-1.js
@@ -0,0 +1,6 @@
+"use strict";
+let name = '"Генератор защитного поля"';
+let price = 1000;
+console.log(`Выбран ${name}, цена за штуку ${price} кредитов`);
+price = 2000;
+console.log(`Выбран ${name}, цена за штуку ${price} кредитов`);
